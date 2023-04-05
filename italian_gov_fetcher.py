908fcd7bdac356c1d7eeb114a04498c34f848f1c
Filipe <76520153+FilipeAlberoPomar@users.noreply.github.com>
2023-04-05 14:04:26 +0100

    Update italian_gov_fetcher.py
    Stable version before major refactoring

--- italian_gov_fetcher.py
+++ italian_gov_fetcher.py
@@ -2,22 +2,21 @@
 # Brace! Brace! Ugly, unifished code below
 # This script downloads and parses financial information from italian cities
 
-import requests
 import os
 from random import randint
 from time import sleep
+import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = 'https://finanzalocale.interno.gov.it/apps/floc.php/certificati/index/codice_ente/{city_code}/cod/4/anno/{year}/md/0/cod_modello/CCOU/tipo_modello/U/cod_quadro/03'
-HEADERS = {
-    'user-agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'}
+CITY_URL = "https://finanzalocale.interno.gov.it/apps/floc.php/indicatori/index/codice_ente/{city_code}/anno/{year}/cod/8/md/0/tipo_certificato/C/tipo_ente/CO"
+HEADERS = {'user-agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'}
 
 cities = {
-    "Alessandria": "1010020030"
+    "Lucca": "3090430170"
 }
 
-START_YEAR = 1996
-END_YEAR = 2009
+START_YEAR = 1998
+END_YEAR = 2011
 
 OUTPUT_FOLDER = 'scrapper'
 
@@ -34,8 +33,7 @@ def fetch():
             file_name = f'{OUTPUT_FOLDER}/{city_name}-{year}.html'
 
             if not os.path.exists(file_name):
-                
-                url = BASE_URL.format(city_code=city_code, year=year)
+                url = CITY_URL.format(city_code=city_code, year=year)
                 print(f"Fetching {city_name} for {year}")
                 response = requests.get(url, headers=HEADERS, timeout=2)
 
@@ -50,24 +48,35 @@ def extract():
     files = os.listdir(OUTPUT_FOLDER)
 
     for file_name in sorted(files):
+        file_name_chunks = file_name.replace('.html','').split('-')
+        city = file_name_chunks[0]
+        year = file_name_chunks[1]
 
         if '.html' in file_name:
             canonical_file = f"{OUTPUT_FOLDER}/{file_name}"
 
-            print(f'Processing {file_name}')
+            #print(f'Processing {file_name}')
             file = open(canonical_file,encoding='iso-8859-1')
             html = file.read()
 
-            if 'Dati assenti' in html:
-                print("no data")
+            csv_line = ""
+
+            if 'Non esistono indicatori finanziari' in html:
+                csv_line = f"{file_name};0;0;0;0"
 
             else:
                 soup = BeautifulSoup(html, 'html.parser')
                 table = soup.find(class_='table table-striped table-bordered table-condensed')
-                money = table.tr.next.next.next.next.next.next.next.next.next.next.next.next.contents[0]
-                print(money)
+                row = table.contents[5]
+                valore_ente = row.contents[1].text.strip()
+                valore_provincia = row.contents[3].text.strip()
+                valore_regione = row.contents[5].text.strip()
+                valore_nazione = row.contents[7].text.strip()
 
-            print("\n")
+                csv_line = f"{city};{year};{valore_ente};{valore_provincia};{valore_regione};{valore_nazione}"
+
+            print(csv_line)
+
 
 
 if __name__ == '__main__':
